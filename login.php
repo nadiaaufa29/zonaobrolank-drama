1fbe30628f584b348cd71e7ff441bac7b68d7d5a
nadiaaufa29 <111202415838@mhs.dinus.ac.id>
2026-01-08 11:29:28 +0700

    ubah judul form login

--- login.php
+++ login.php
@@ -47,7 +47,7 @@ if (isset($_SESSION['username'])) {
                         <div class="card-body">
                             <div class="text-center mb-3">
                                 <i class="bi bi-person-circle h1 display-4"></i>
-                                <p>Zona Obrolan K-Drama</p>
+                                <p>Welcome to Zona Obrolan K-Drama</p>
                                 <hr />
                             </div>
                             <form action="" method="post" id="loginForm">
